feff75e593721357665f1b0a68d7a9e31c154f7d
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2019-09-22 01:54:01 -0400

    Implement crawler with producer/consumer pattern

--- crawler.py
+++ crawler.py
@@ -1,7 +1,7 @@
-import concurrent.futures
 import logging
 import queue
 import threading
+import time
 from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
@@ -12,161 +12,136 @@ import tldextract
 from seeders import DuckDuckGoSeeder
 
 
+logging.basicConfig(format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+
+def get_domain(url):
+    tld = tldextract.extract(url)
+    return f'{tld.domain}.{tld.suffix}'
 
 
 class Crawler:
-    def __init__(self, urls):
-        self.urls = urls
+    def __init__(self):
+        self.name = 'Crawler'
+        self.valid_url_queue = queue.Queue()
+        self.candidate_url_queue = queue.Queue()
         self.num_workers = 3
-        self.max_visits_per_domain = 3
-        self.user_agent = 'mvp307'
-        self.active_domains = set()
-        self.crawled_urls = set()
-        self.crawled_domains = {}
+        self.domain_locks = {}
 
-    def crawl(self):
-        # while not self.is_crawl_finished():
-        for _ in range(5):
-            logger.debug(f'URLs: {self.urls[:5]}')
-            crawl_urls = self.get_urls_to_crawl()
-            logger.debug(f'Active domains: {self.active_domains}')
-            returned_urls = self.crawl_pages(crawl_urls)
-            self.update_crawled_urls(crawl_urls)
-            self.update_urls_to_crawl(returned_urls)
+    def crawl(self, urls):
+        logger.debug(f'{self.name} - Starting crawler')
 
-    def is_crawl_finished(self):
-        return len(self.urls) == 0
-
-    def get_urls_to_crawl(self):
-        crawl_urls = []
-        self.active_domains = set()
-
-        while self.urls:
-            url = self.urls.pop(0)
-            domain = self.get_domain(url)
-
-            if domain in self.active_domains:
-                logger.debug(f'URL {url} skipped because domain {domain} is active')
-                self.urls.append(url)
-                continue
-
-            if url in crawl_urls:
-                logger.debug(f'URL {url} skipped because it is already in the current list of crawled URLs')
-                continue
-
-            if self.is_domain_saturated(domain):
-                logger.debug(f'URL {url} skipped because domain {domain} has been visited too many times')
-                continue
-
-            crawl_urls.append(url)
-            self.active_domains.add(domain)
-            self.update_crawled_domain_count(domain)
-
-            if len(crawl_urls) == self.num_workers:
-                break
-
-        return crawl_urls
-
-    def update_active_domains(self, urls):
         for url in urls:
-            self.update_active_domain(url)
+            self.enqueue_url(url)
 
-    def update_active_domain(self, url):
-        domain = self.get_domain(url)
-        self.active_domains.add(domain)
+        self.spawn_url_validator()
+        self.spawn_workers()
 
-    def is_domain_active(self, domain):
-        return domain in self.active_domains
+        time.sleep(1)
 
-    def clear_active_domains(self):
-        self.active_domains = set()
+        self.valid_url_queue.join()
 
-    def get_domain(self, url):
-        tld = tldextract.extract(url)
-        return f'{tld.domain}.{tld.suffix}'
+    def enqueue_url(self, url):
+        self.candidate_url_queue.put(url)
 
-    def is_url_crawled(self, url):
-        return url in self.crawled_urls
+    def spawn_url_validator(self):
+        logger.debug(f'{self.name} - Spawning URL validator')
+        validator = URLValidatorThread(self.valid_url_queue, self.candidate_url_queue, self.domain_locks)
+        validator.start()
 
-    def is_domain_saturated(self, domain):
-        return self.crawled_domains.get(domain, 0) == self.max_visits_per_domain
+    def spawn_workers(self):
+        for worker_id in range(self.num_workers):
+            logger.debug(f'{self.name} - Spawning crawler worker')
+            worker = WorkerThread(worker_id, self.valid_url_queue, self.candidate_url_queue, self.domain_locks)
+            worker.start()
 
-    def update_crawled_domain_count(self, domain):
-        count = self.crawled_domains.get(domain, 0)
-        count += 1
-        self.crawled_domains[domain] = count
-        logger.debug(f'Domain {domain} visits: {count}')
 
-    def update_crawled_urls(self, urls):
-        for url in urls:
-            self.crawled_urls.add(url)
+class WorkerThread(threading.Thread):
+    def __init__(self, worker_id, valid_url_queue, candidate_url_queue, domain_locks):
+        super().__init__(daemon=True)
+        self.name = f'Worker {worker_id}'
+        self.valid_url_queue = valid_url_queue
+        self.candidate_url_queue = candidate_url_queue
+        self.domain_locks = domain_locks
+        self.user_agent = 'mvp'
 
-    def update_urls_to_crawl(self, urls):
-        for url in urls:
-            if not self.is_url_crawled(url):
-                self.urls.append(url)
+        logger.debug(f'{self.name} - Spawned')
 
-    def crawl_pages(self, urls):
-        logger.info(f'Crawling {len(urls)} pages')
+    def run(self):
+        for url in self.enqueued_valid_urls():
+            self.crawl_url(url)
 
-        crawled_urls = []
+    def enqueued_valid_urls(self):
+        while True:
+            url = self.valid_url_queue.get()
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_workers) as executor:
-            futures = [executor.submit(self.crawl_page, url) for url in urls]
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    base_url, urls = future.result()
-                except Exception as e:
-                    logger.error(f'Error {e}')
-                else:
-                    logger.info(f'{base_url} has {len(urls)} URLs: {urls[:10]}')
-                    crawled_urls.extend(urls)
+            # Avoid simultaneous accesses to same domain
+            lock = self.get_domain_lock(url)
+            with lock:
+                logger.debug(f'{self.name} - Domain locks: {self.domain_locks}')
+                yield url
 
-        return crawled_urls
+            self.valid_url_queue.task_done()
 
-    def crawl_page(self, url):
-        logger.debug(f'Crawling {url}')
-        if not self.is_url_crawlable(url):
-            logger.debug(f'Skipping {url} following robots.txt rules')
-            return url, []
+    def get_domain_lock(self, url):
+        domain = get_domain(url)
+        return self.domain_locks[domain]
+
+    def crawl_url(self, url):
+        logger.debug(f'{self.name} - Started crawling URL {url}')
+        logger.info(url)
+
+        if not self.is_robots_allowed(url):
+            return
 
         page = self.fetch_page(url)
-        urls = self.extract_urls(page)
-        normalized_urls = list(set(self.normalize_urls(url, urls)))
+        if page is None:
+            return
 
-        logger.info(f'Base URL: {url}')
-        logger.info(f'URLs: {urls[:10]}')
-        logger.info(f'Normalized URLs: {normalized_urls[:10]}')
+        candidate_urls = self.extract_urls(page)
+        candidate_urls = self.normalize_urls(url, candidate_urls)
+        candidate_urls = self.deduplicate_urls(candidate_urls)
+        self.enqueue_candidate_urls(candidate_urls)
 
-        return url, normalized_urls
+        logger.debug(f'{self.name} - Finished crawling URL {url}')
 
-    def is_url_crawlable(self, url):
-        robots = reppy.Robots.fetch(reppy.Robots.robots_url(url))
-        return robots.allowed(url, self.user_agent)
+    def is_robots_allowed(self, url):
+        try:
+            robots = reppy.Robots.fetch(reppy.Robots.robots_url(url))
+            return robots.allowed(url, self.user_agent)
+        except reppy.exceptions.ReppyException as e:
+            logger.debug(f'{self.name} - Error when reading robots for URL {url} - {e}')
+            return
 
     def fetch_page(self, url):
         headers = {
             'User-Agent': self.user_agent,
         }
-        response = requests.get(url, headers=headers)
-        return response.content
+
+        try:
+            response = requests.get(url, headers=headers)
+            return response.content
+        except requests.exceptions.RequestException as e:
+            logger.debug(f'{self.name} - Error when crawling URL {url} - {e}')
+            return None
 
     def extract_urls(self, page):
         soup = BeautifulSoup(page, 'html.parser')
         links = soup.find_all('a')
         urls = [link.get('href') for link in links]
+
+        # Remove None from list
         urls = list(filter(lambda u: u != None, urls))
         return urls
 
-    def normalize_urls(self, base_url, urls):
-        return [self.normalize_url(base_url, url) for url in urls]
+    def normalize_urls(self, base_url, candidate_urls):
+        return [self.normalize_url(base_url, candidate_url) for candidate_url in candidate_urls]
 
-    def normalize_url(self, base_url, url):
+    def normalize_url(self, base_url, candidate_url):
         parsed_base_url = urlparse(base_url)
-        parsed_url = urlparse(url)
+        parsed_url = urlparse(candidate_url)
 
         scheme = parsed_url.scheme or parsed_base_url.scheme
         domain = parsed_url.netloc or parsed_base_url.netloc
@@ -178,24 +153,127 @@ class Crawler:
         normalized_url = f'{scheme}://{domain}{path}{query}'
         return normalized_url
 
+    def deduplicate_urls(self, urls):
+        return list(set(urls))
+
+    def enqueue_candidate_urls(self, candidate_urls):
+        for url in candidate_urls:
+            self.candidate_url_queue.put(url)
+
+
+class URLValidatorThread(threading.Thread):
+    def __init__(self, valid_url_queue, candidate_url_queue, domain_locks):
+        super().__init__(daemon=True)
+        self.name = 'URLValidator'
+
+        self.valid_url_queue = valid_url_queue
+        self.candidate_url_queue = candidate_url_queue
+
+        self.domain_locks = domain_locks
+
+        self.validators = [
+            TooManyDomainAccessesValidator(),
+            URLAlreadyVisitedValidator(),
+        ]
+
+        logger.debug(f'{self.name} - Spawned')
+
+    def run(self):
+        while True:
+            candidate_url = self.candidate_url_queue.get()
+            self.process_candidate_url(candidate_url)
+
+    def process_candidate_url(self, candidate_url):
+        logger.debug(f'{self.name} - Validating URL {candidate_url}')
+
+        if self.is_valid_url(candidate_url):
+            for validator in self.validators:
+                validator.update(candidate_url)
+
+            self.ensure_domain_lock_exists(candidate_url)
+
+            # Add to queue only *after* updating validators to avoid processing
+            # URLs that are still being validated
+            self.valid_url_queue.put(candidate_url)
+
+    def is_valid_url(self, candidate_url):
+        for validator in self.validators:
+            if not validator.is_valid(candidate_url):
+                logger.debug(f'{self.name} - Skipping URL {candidate_url} - {validator.__class__.__name__}')
+                return False
+
+        logger.debug(f'{self.name} - Enqueuing URL {candidate_url}')
+        return True
+
+    def ensure_domain_lock_exists(self, url):
+        domain = get_domain(url)
+        if domain not in self.domain_locks:
+            self.domain_locks[domain] = threading.Lock()
+
+        logger.debug(f'{self.name} - Domain locks: {self.domain_locks}')
+
+
+class URLValidator:
+    def is_valid(self, candidate_url):
+        raise NotImplementedError
+
+    def update(self, url):
+        raise NotImplementedError
+
+
+class URLAlreadyVisitedValidator(URLValidator):
+    def __init__(self):
+        self.visited_urls = set()
+
+    def is_valid(self, candidate_url):
+        return candidate_url not in self.visited_urls
+
+    def update(self, url):
+        self.visited_urls.add(url)
+
+
+class TooManyDomainAccessesValidator(URLValidator):
+    def __init__(self):
+        self.domain_accesses = {}
+        self.max_accesses = 3
+
+    def is_valid(self, candidate_url):
+        domain = get_domain(candidate_url)
+        accesses = self.domain_accesses.get(domain, 0)
+        return accesses < self.max_accesses
+
+    def update(self, url):
+        domain = get_domain(url)
+        accesses = self.domain_accesses.get(domain, 0)
+        accesses += 1
+        self.domain_accesses[domain] = accesses
+
 
 def main():
     query = 'dogs'
     logger.info(f'Crawling "{query}"')
 
-    # seeder = DuckDuckGoSeeder()
-    # urls = seeder.get_urls(query)
-    # logger.debug(urls)
-    urls = [
-        'https://en.wikipedia.org/wiki/Dog',
-        'https://www.dictionary.com/browse/dogs',
-        'https://www.adoptapet.com/',
-        'http://www.rescueme.org/',
-        'https://www.petfinder.com/dogs/',
-    ]
+    seeder = DuckDuckGoSeeder()
+    urls = seeder.get_urls(query)
+    logger.debug(urls)
 
-    crawler = Crawler(urls)
-    crawler.crawl()
+    # urls = [
+    #     'https://en.wikipedia.org/wiki/Dog',
+    #     'https://en.wikipedia.org/wiki/Cat',
+    #     'https://en.wikipedia.org/wiki/Parrot',
+    #     'https://en.wikipedia.org/wiki/Turtle',
+    #     'https://en.wikipedia.org/wiki/Armadillo',
+    #     'https://en.wikipedia.org/wiki/Snake',
+    #     'https://www.dictionary.com/browse/dogs',
+    #     'https://www.adoptapet.com/',
+    #     'https://en.wikipedia.org/wiki/Pet',
+    #     'https://en.wikipedia.org/wiki/Brazil',
+    #     'https://en.wikipedia.org/wiki/Tenis',
+    #     'http://www.rescueme.org/',
+    #     'https://www.petfinder.com/dogs/',
+    # ]
+    crawler = Crawler()
+    crawler.crawl(urls)
 
 
 if __name__ == '__main__':
